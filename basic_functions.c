ff1c1eeff6772e626dd7dbf5e73fc139a7a5ce03
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-03-28 19:31:52 +0530

    added is_odd

--- basic_functions.c
+++ basic_functions.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
 unsigned char is_even(int);
+unsigned char is_odd(int);
 
 unsigned char is_even(int number){
   return number % 2 == 0 ? 'T' : 'F';
+}
+
+unsigned char is_odd(int number){
+  return is_even(number) == 'T' ? 'F' : 'T';
 }
 
 int main(void)
 {
   printf("is 2 Even? %c\n", is_even(2));
   printf("is 1 Even? %c\n", is_even(1));
+
+  printf("is 1 Odd? %c\n", is_odd(1));
+  printf("is 2 Odd? %c\n", is_odd(2));
+
   return 0;
 }
