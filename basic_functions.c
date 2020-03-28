2bf5f9999ab456786948bee8102e37215c78cda7
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-03-28 19:53:45 +0530

    added square

--- basic_functions.c
+++ basic_functions.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <math.h>
 
 unsigned char is_even(int);
 unsigned char is_odd(int);
+double square(double);
 
 unsigned char is_even(int number){
   return number % 2 == 0 ? 'T' : 'F';
@@ -11,6 +13,10 @@ unsigned char is_odd(int number){
   return is_even(number) == 'T' ? 'F' : 'T';
 }
 
+double square(double number){
+  return pow(number, 2);
+}
+
 int main(void)
 {
   printf("is 2 Even? %c\n", is_even(2));
@@ -18,6 +24,9 @@ int main(void)
 
   printf("is 1 Odd? %c\n", is_odd(1));
   printf("is 2 Odd? %c\n", is_odd(2));
+
+  printf("Square of 2: %lf\n", square(2));
+  printf("Square of 2.2: %lf\n", square(2.2));
 
   return 0;
 }
