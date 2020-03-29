acc7f88f74daee2e3e67ee8b505b49e79827c72b
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-03-29 08:18:43 +0530

    added gcd

--- src/basic_functions.c
+++ src/basic_functions.c
@@ -5,6 +5,7 @@ unsigned char is_even(int);
 unsigned char is_odd(int);
 double square(double);
 double cube(double);
+unsigned gcd(unsigned, unsigned);
 
 unsigned char is_even(int number){
   return number % 2 == 0;
@@ -22,9 +23,32 @@ double cube(double number){
   return square(number) * number;
 }
 
+unsigned gcd(unsigned num1, unsigned num2){
+  unsigned greatest_common_divisor;
+  unsigned dividend = num1;
+  unsigned divisor = num2;
+
+  if(num2 > num1)
+  {
+    dividend = num2;
+    divisor = num1;
+  }
+
+  greatest_common_divisor = dividend;
+
+  while(divisor != 0)
+  {
+    greatest_common_divisor = divisor;
+    divisor = dividend % divisor;
+    dividend = greatest_common_divisor;
+  }
+
+  return greatest_common_divisor;
+}
+
 int main(void)
 {
-  int number;
+  int number, number2;
 
   printf("1. isEven\n");
   printf("Enter a Number: ");
@@ -45,6 +69,11 @@ int main(void)
   printf("Enter a Number: ");
   scanf("%d", &number);
   printf("%d: %lf\n\n", number, cube(number));
+
+  printf("5. gcd\n");
+  printf("Enter two positive numbers: ");
+  scanf("%u %u", &number, &number2);
+  printf("gcd(%u, %u): %u\n", number, number2, gcd(number, number2));
 
   return 0;
 }
