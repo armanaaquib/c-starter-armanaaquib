857ebd336975581c7b915aa801c488990f96c876
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-03-29 17:33:31 +0530

    added Even Number Series

--- src/loops.c
+++ src/loops.c
@@ -3,6 +3,7 @@
 unsigned long long factorial(unsigned);
 void print_fibonacci(unsigned);
 void print_odd_numbers(unsigned);
+void print_even_numbers(unsigned);
 
 unsigned long long factorial(unsigned number)
 {
@@ -40,6 +41,16 @@ void print_odd_numbers(unsigned number)
   }
 }
 
+void print_even_numbers(unsigned number)
+{
+  unsigned even_number;
+
+  for(even_number = 0; even_number <= number; even_number += 2)
+  {
+    printf("%d ", even_number);
+  }
+}
+
 int main(void)
 {
   unsigned number;
@@ -59,6 +70,12 @@ int main(void)
   printf("Enter a positive number: ");
   scanf("%u", &number);
   print_odd_numbers(number);
+  printf("\n\n");
+
+  printf("4. Even Number Series");
+  printf("Enter a positive number: ");
+  scanf("%u", &number);
+  print_even_numbers(number);
   printf("\n\n");
 
   return 0;
