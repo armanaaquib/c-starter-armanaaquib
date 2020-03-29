2fcc1d91781d1c88509e92a5b6ffc550e74afcaa
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-03-29 13:08:17 +0530

    added fibonacci

--- src/loops.c
+++ src/loops.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 unsigned long long factorial(unsigned);
+void print_fibonacci(unsigned);
 
 unsigned long long factorial(unsigned number)
 {
@@ -15,6 +16,19 @@ unsigned long long factorial(unsigned number)
   return fact;
 }
 
+void print_fibonacci(unsigned number)
+{
+  unsigned term1 = 0, term2 = 1, count, sum;
+
+  for(count = 0; count < number; count++)
+  {
+    printf("%d ", term1);
+    sum = term1 + term2;
+    term1 = term2;
+    term2 = sum;
+  }
+}
+
 int main(void)
 {
   unsigned number;
@@ -23,6 +37,12 @@ int main(void)
   printf("Enter a Numbers: ");
   scanf("%u", &number);
   printf("factorial(%u): %llu\n\n", number, factorial(number));
+
+  printf("2. Fibonacci\n");
+  printf("Enter a Count: ");
+  scanf("%d", &number);
+  print_fibonacci(number);
+  printf("\n\n");
 
   return 0;
 }
