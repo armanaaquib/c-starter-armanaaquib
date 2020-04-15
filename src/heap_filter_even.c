d94028615789e6c0e4a8f00d866788d73a46ede4
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-04-15 19:44:22 +0530

    takes: user input

--- src/heap_filter_even.c
+++ src/heap_filter_even.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ITERATE(start, end) for(unsigned index = start; index < end; index++)
+
 typedef struct
 {
   int length;
@@ -10,6 +12,8 @@ typedef struct
 
 Evens *filter_even(int *, unsigned);
 unsigned char is_even(int);
+void print_numbers(int *, unsigned);
+void input_numbers(int *, unsigned);
 
 unsigned char is_even(int number){
   return number % 2 == 0;
@@ -21,7 +25,7 @@ Evens *filter_even(int *numbers, unsigned count)
   unsigned no_of_even_nos = 0, temp_index = 0;
   int *even_numbers;
 
-  for(int index = 0; index < count; index++)
+  ITERATE(0, count)
   {
     int number = numbers[index];
 
@@ -34,7 +38,7 @@ Evens *filter_even(int *numbers, unsigned count)
 
   even_numbers = malloc(sizeof(int) * no_of_even_nos);
 
-  for(int index = 0; index < no_of_even_nos; index++)
+  ITERATE(0, no_of_even_nos)
   {
     even_numbers[index] = temp_evens[index];
   }
@@ -46,17 +50,40 @@ Evens *filter_even(int *numbers, unsigned count)
   return evens;
 }
 
-int main(void)
+void print_numbers(int *array, unsigned length)
 {
-  int numbers[] = {1, 2, 3, 4};
-
-  Evens * evens = filter_even(numbers, 4);
-
-  for(int index = 0; index < evens->length; index++)
+  ITERATE(0, length)
   {
-    printf("%d ", evens->numbers[index]);
+    printf("%d ", array[index]);
   }
   printf("\n");
-  
+}
+
+void input_numbers(int *array, unsigned length)
+{
+  printf("Enter %d numbers: ", length);
+  ITERATE(0, length)
+  {
+    scanf("%d,", &array[index]);
+  }
+}
+
+int main(void)
+{
+  unsigned length;
+  int *numbers;
+
+  printf("Enter length: ");
+  scanf("%d", &length);
+
+  numbers = malloc(sizeof(int) * length);
+
+  input_numbers(numbers, length);
+
+  Evens * evens = filter_even(numbers, length);
+
+  printf("Even Numbers: ");
+  print_numbers(evens->numbers, evens->length);
+
   return 0;
 }
